c1f873f7b697d710c4b7abbdf8fa797baf3ae716
Cirdes Henrique <cirdes@eventick.com.br>
2012-12-19 13:08:47 -0300

    accept crossdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	after_filter :set_access_control_headers
+
+	def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE'
+  end
 end
